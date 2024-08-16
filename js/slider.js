17365429664546616d95a92312f8f8d1e535b781
sankaku0724 <yuto58836@gmail.com>
2024-08-16 16:55:58 +0900

    Update homepage 8/16 v2

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,35 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+//     if($(window).width() >= 768){ 
+//         $('.pc-slideshow').bxSlider({
+//             auto: true,
+//             pause: 5000
+//         });
+//     } else { 
+//         $('.mobile-slideshow').bxSlider({
+//             auto: true,
+//             pause: 5000
+//         });
+//     }
+// });
+
+$(document).ready(function() {
+    var pcSliderOptions = {
+        auto: true,
+        pause: 5000,
+        adaptiveHeight: true
+    };
+
+    var mobileSliderOptions = {
+        auto: true,
+        pause: 5000,
+        adaptiveHeight: true
+    };
+
     if($(window).width() >= 768){ 
-        $('.pc-slideshow').bxSlider({
-            auto: true,
-            pause: 5000
-        });
+        $('.pc-slideshow').bxSlider(pcSliderOptions);
     } else { 
-        $('.mobile-slideshow').bxSlider({
-            auto: true,
-            pause: 5000
-        });
+        $('.mobile-slideshow').bxSlider(mobileSliderOptions);
     }
 });
-
 
 
