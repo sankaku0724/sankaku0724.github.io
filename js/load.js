0322f22b0bd59fae833b76d016e398990d840a39
sankaku0724 <yuto58836@gmail.com>
2024-08-29 11:46:19 +0900

    Update homepage 8/29 v2

--- js/load.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  const [navigation] = performance.getEntriesByType("navigation");
-  if (navigation.type !== 'reload') {
-    setTimeout(function() {
-      location.reload(true);
-    }, 100);
-  }
-});
